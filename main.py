afc2154be77abecb617f50e16996e2faa548d203
Yiorgos Gravalos <105599202+giorgosgrv3@users.noreply.github.com>
2025-03-07 20:14:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def mhtsotakhGamiesai(roufianos1, roufianos2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Mesa se ena sako valte {roufianos1} kai {roufianos2}')  # Press F9 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    rouf1 = input ("Dwse politiko apatewna: ")
+    rouf2 = input ("Dwse roufianoARD tou Skai: ")
+    mhtsotakhGamiesai(rouf1, rouf2)

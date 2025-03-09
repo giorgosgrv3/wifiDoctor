e091bb66d7fcd7a9d8e52874623757939d7dd0c6
Giwrgaras <ggravalos@tuc.gr>
2025-03-09 05:10:19 +0200

    allagoules

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-# This is a sample Python script.
+import pyshark
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = pyshark.FileCapture("random.pcapng")
 
-
-def mhtsotakhGamiesai(roufianos1, roufianos2):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Mesa se ena sako valte {roufianos1} kai {roufianos2}')  # Press F9 to toggle the breakpoint.
-    print("Piph stamou eisai magkas")
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-        rouf1 = input ("Dwse politiko apatewna: ")
-        rouf2 = input ("Dwse roufianoARD tou Skai: ")
-        mhtsotakhGamiesai(rouf1, rouf2)
-        
+print(cap[0])
         

c03f6376f0d6534f54cd25c687b9c0303b7fcaf4
giwrgaras <ggravalos@tuc.gr>
2025-03-20 18:23:12 +0200

    pipis

--- main.py
+++ main.py
@@ -1,64 +1,83 @@
 import pyshark
+import pandas as pd
 
-cap = pyshark.FileCapture('random.pcapng', display_filter='wlan.fc.type_subtype == 0x08')
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 1000)
+pd.set_option("display.colheader_justify", "left")
+pd.set_option("display.expand_frame_repr", False)
+
+
+'''======================================================PCAP PARSER========================================================================'''
 
 def hex_ssid_to_string(hex_ssid):
-    # Remove colons and convert hex to bytes
     hex_str = hex_ssid.replace(":", "")
     return bytes.fromhex(hex_str).decode('utf-8', errors='ignore')
 
+def pcap_Parser(file):
+
+    captures = []
+    cap = pyshark.FileCapture(file, display_filter='wlan.fc.type_subtype == 0x08')
+
+    for packet in cap:
+        try:
+
+            if 'wlan.mgt' in packet:
+                hex_ssid = getattr(packet['wlan.mgt'], 'wlan_ssid', 'Could not be found.')
+                ssid = hex_ssid_to_string(hex_ssid)
+                tsf = getattr(packet['wlan.mgt'], 'fixed_timestamp', 'Not available')
+            else:
+                ssid = 'Not available'
+                tsf = 'Not available'
+
+            bssid = getattr(packet['wlan'], 'bssid', 'Could not be found.')
+            ra = getattr(packet['wlan'], 'ra', 'Could not be found.')
+            ta = getattr(packet['wlan'], 'ta', 'Could not be found.')
+            type_subtype = getattr(packet['wlan'], 'fc_type_subtype', 'Could not be found.')
+            phy = getattr(packet['wlan_radio'], 'phy', 'Could not be found.')
+            mcs_idx = getattr(packet['wlan_radio'], 'mcs_idx', 'Could not be found.')
+            bandwidth = getattr(packet['wlan_radio'], 'bandwidth', 'Could not be found.')
+            spatial_strms = getattr(packet['wlan_radio'], 'spatial_strms', 'Could not be found.')
+            channel = getattr(packet['wlan_radio'], 'channel', 'Could not be found.')
+
+            try:
+                freq = float(packet['wlan_radio'].frequency)
+            except (AttributeError, ValueError):
+                freq = 0.0
+
+            speed = float(getattr(packet['wlan_radio'], 'data_rate', '0.0'))
+            strength = getattr(packet['wlan_radio'], 'signal_dbm', 'Could not be found.')
+            shortgi = getattr(packet["radiotap"], "flags_shortgi", "Could not be found.")
+
+            band = "2.4 GHz" if freq < 3000 else "5 GHz"
+
+            captures.append({
+                "BSSID": bssid,
+                "SSID": ssid,
+                "Receiver Mac": ra,
+                "Transmitter Mac": ta,
+                "Type/Subtype": type_subtype,
+                "PHY Type": phy,
+                "MCS index": mcs_idx,
+                "Bandwidth": bandwidth,
+                "Spatial streams": spatial_strms,
+                "Channel": channel,
+                "Frequency (MHz)": freq,
+                "Data Rate (Mbps)": speed,
+                "Signal Strength (dBm)": strength,
+                "Short GI": shortgi,
+                "TSF timestamp": tsf,
+                "Band": band
+            })
+        except AttributeError:
+            continue
+
+    cap.close()
+    return pd.DataFrame(captures)
+
+'''======================================================================================================================================'''
 
 
-print(cap[0])
 
-for packet in cap:
-    if 'wlan.mgt' in packet:
-        print("******* radiotap *******:", packet['radiotap'].field_names)
-        print("******* wlan_radio *******:", packet['wlan_radio'].field_names)
-        print("***** wlan.mgt *****:", packet['wlan.mgt'].field_names)
-        print("******* wlan *******:", packet['wlan'].field_names)
-        break  
-
-for packet in cap:
-    print(f"Packet layers: {[layer.layer_name for layer in packet.layers]}")
-    break  
-
-for pkt in cap:
-        
-        hex_ssid = getattr(pkt['wlan.mgt'], 'wlan_ssid', 'Could not be found.')
-        #hex_ssid = pkt['wlan.mgt'].wlan_ssid XWRIS TO getattr(), apla xrhsimopoioume getattr() gia safety an den uparxei h timh
-        ssid = hex_ssid_to_string(hex_ssid)
-
-        #bssid = pkt['wlan'].bssid XWRIS TO getattr(), paromoiws kai gia ta upoloipa
-        bssid = getattr(pkt['wlan'], 'bssid', 'Could not be found.')
-
-        ra = getattr(pkt['wlan'], 'ra', 'Could not be found.')
-
-        ta = getattr(pkt['wlan'], 'ta', 'Could not be found.')
-
-        phy = getattr(pkt['wlan_radio'], 'phy', 'Could not be found.')
-
-        channel = getattr(pkt['wlan_radio'], 'channel', 'Could not be found.')
-        freq = getattr(pkt['wlan_radio'], 'frequency', 'Could not be found.')
-
-        speed = float(getattr(pkt['wlan_radio'], 'data_rate', 'Could not be found.'))
-
-        strength = getattr(pkt['wlan_radio'], 'signal_dbm', 'Could not be found.')
-
-        shortgi = getattr(pkt["radiotap"], "flags_shortgi", "Could not be found.")
-
-        break
-
-
-# Print in an aligned format
-print(f"{'SSID:':<23}{ssid}")
-print(f"{'BSSID:':<23}{bssid}")
-print(f"{'Broadcast MAC (ra):':<23}{ra}")
-print(f"{'Transmitter MAC (ta):':<23}{ta}")
-print(f"{'PHY:':<23}{phy}")
-print(f"{'Channel:':<23}{channel}")
-print(f"{'Frequency:':<23}{freq} MHz")
-print(f"{'Data Rate:':<23}{speed:.1f} Mb/s") 
-print(f"{'Signal Strength:':<23}{strength} dBm")
-print(f"{'Short GI:':<23}{shortgi}")
-
+df = pcap_Parser("TUC.pcapng")
+print(df)
